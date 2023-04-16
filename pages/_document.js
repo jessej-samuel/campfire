c348274244f3b5ffd06c5a003f1e54257f17b559
jessej-samuel <1234jessej@gmail.com>
2023-04-16 22:07:17 +0530

    feat: Navbar

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body>
+      <body className="scroll-smooth bg-white dark:bg-black text-black dark:text-white">
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
+import { useWindowScroll } from "react-use";
 
 export default function Home() {
+  const { x, y } = useWindowScroll();
+  const navRef = useRef();
+
+  const NAVBG = "bg-inherit";
+
+  useEffect(() => {
+    if (y > 0) {
+      navRef.current.classList.remove(NAVBG);
+      navRef.current.classList.add("bg-black");
+      navRef.current.classList.add("text-white");
+    } else {
+      navRef.current.classList.add(NAVBG);
+      navRef.current.classList.remove("bg-black");
+      navRef.current.classList.remove("text-white");
+    }
+  }, [y]);
+
   return (
-    <div>
-      <nav className="border-b shadow">
-        <Link href="/" className="flex items-center text-lg">
+    <>
+      <nav
+        ref={navRef}
+        className={`sticky top-0 flex justify-between py-2 items-center transition-colors ${NAVBG}`}
+      >
+        <Link href="/" className="flex items-center  text-lg font-serif">
           <Image src="/images/logo.png" alt="Logo" width={48} height={48} />
           Campfire
         </Link>
+        <ul className="text-sm font-medium text-opacity-0 flex mx-8">
+          <li className="mr-4 hover:text-blue-500 ">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="mr-4 hover:text-blue-500 ">
+            <Link href="/contact" className="mr-4">
+              Contact
+            </Link>
+          </li>
+        </ul>
       </nav>
-    </div>
+      {/* TODO: Hero Section */}
+    </>
   );
 }
